9d47aa1cf4ad1b0a1faaa8ff6abae2ea51e3b1e3
Raphael Pfaff <pfaff@fh-aachen.de>
2019-03-07 21:20:09 +0100

    Fiddled...

--- PMSM/RunPMSM.m
+++ PMSM/RunPMSM.m
@@ -4,24 +4,24 @@ clear all, close all, clc
 
 %% Run PMSM
 % System parameters
-R1 = 10;
+R1 = 0.1;
 L1q = 1e-3;
 L1d = 1e-4;
-psi = 10;
+psi = .1;
 p = 1;
-J = 10;
+J = 1;
 kp = 100;
-kpw = 100;
+kpw = 1;
 
 % Input signal
 n = 500;
 tmax = 300;
 simin.time = linspace(0,tmax,n);
-simin.signals.values= [300*ones(n,1), zeros(n,1), [zeros(n/2,1); 1*ones(n/2,1)]];
+simin.signals.values= [30*ones(n,1), zeros(n,1), [zeros(n/2,1); 1*ones(n/2,1)]];
 var.signals.dimensions=[n,3];
 
 sim('PMSM.slx')
 figure
 plot(simout.time, simout.Data)
-legend('\omega', 'M', 'i', 'u')
+legend('\omega', 'T', 'i_d', 'i_q')
 
