943716172e2170711d79a9e02479192323cbfd1c
Raphael Pfaff <pfaff@fh-aachen.de>
2019-03-07 11:47:23 +0100

    Added cascade controller

--- PMSM/RunPMSM.m
+++ PMSM/RunPMSM.m
@@ -5,17 +5,19 @@ clear all, close all, clc
 %% Run PMSM
 % System parameters
 R1 = 10;
-L1q = 0.1;
-L1d = 0.01;
-psi = 1;
+L1q = 1e-3;
+L1d = 1e-4;
+psi = 10;
 p = 1;
-J = 100;
+J = 10;
+kp = 100;
+kpw = 100;
 
 % Input signal
 n = 500;
 tmax = 300;
 simin.time = linspace(0,tmax,n);
-simin.signals.values= [300*ones(n,1), ones(n,1), [zeros(n/2,1); 100*ones(n/2,1)]];
+simin.signals.values= [300*ones(n,1), zeros(n,1), [zeros(n/2,1); 1*ones(n/2,1)]];
 var.signals.dimensions=[n,3];
 
 sim('PMSM.slx')
